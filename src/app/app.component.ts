85a27d7f57c19c885d602e016731104de37d9563
Dinesh-Dcrew <dineshkumar.subramanian@excelenciaconsulting.com>
2020-04-13 17:54:26 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testImage';
+  testForm: FormGroup
+  file:any
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) {
+    this.testForm = this.fb.group({
+      clientId: '',
+      location: '',
+      meetingPoc: '',
+      meetingPocEmail: '',
+      meetingPocNumber: '',
+      meetingDesig: '',
+      userId: '',
+      meetingMom: '',
+      meetingDate: '',
+      remainderOn: '',
+    })
+  }
+
+  submitData() {
+    console.log("form group", this.testForm.value)
+    var formData = new FormData()
+    for(let obj in this.testForm.value) {
+      formData.append(obj, this.testForm.get(obj).value)
+    }
+    formData.append('files', this.file[0])
+    console.log('posdat', formData);
+    this.httpClient.post('http://localhost:8841/api/ClientMeeting/CreateClientMeeting', formData).subscribe(res => {
+       console.log("response", res)
+    })
+  }
+
+  filevent(event) {
+    console.log("event", event.target.files)
+    this.file = event.target.files
+  }
+
 }
